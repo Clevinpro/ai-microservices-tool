6910bcfc7cbace14fa2b53b453f75bc3beee9089
Bannyi Taras <clevin.pro@gmail.com>
2026-04-28 18:25:18 +0300

    feat: add huskyyyyyy

---
+++ commitlint.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'chore', 'refactor', 'test']],
+    'scope-empty': [1, 'never'],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+  },
+};
